36d18e633927dfe7cfc3816935e4998f5393a2c2
Rajat Garg <gargrajat353@gmail.com>
2022-10-09 05:09:17 +0530

    Project Initialized

---
+++ src/main/java/com/testgeek/testgeek/TestgeekApplication.java
@@ -0,0 +1,14 @@
+package com.testgeek.testgeek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestgeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestgeekApplication.class, args);
+		System.out.println("Started...");
+	}
+
+}
---
+++ src/test/java/com/testgeek/testgeek/TestgeekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testgeek.testgeek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestgeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
